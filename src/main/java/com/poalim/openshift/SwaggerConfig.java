6cb1150c58ddac966924d395aa4e76b933c3f77e
0038 - אושר אטיאס <osher.atias@bnhp>
2017-07-24 09:55:14 +0300

    only show the api in swagger

--- src/main/java/com/poalim/openshift/SwaggerConfig.java
+++ src/main/java/com/poalim/openshift/SwaggerConfig.java
@@ -2,11 +2,13 @@ package com.poalim.openshift;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.RequestHandlerSelectors.withClassAnnotation;
 
 /**
  * Created by osher on 19/7/17.
@@ -18,7 +20,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(withClassAnnotation(RestController.class))
                 .paths(PathSelectors.any())
                 .build();
     }
